2c805a7837e79b1fc9aa53f605033dd33c89675b
nacsha <nacshapg@gmail.com>
2026-07-24 17:22:16 +0900

    bisection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 fn main () {
-    let mut x_left: f64 = 0.0;
-    let mut x_right: f64 = 1.0;
+    let x_left_ini: f64 = 0.0;
+    let x_right_ini: f64 = 1.0;
 
-    let mut x_mid: f64 = (x_left + x_right) * 0.5;
+    let ans: Option<f64> = recur(x_left_ini, x_right_ini);
 
-    println!("Hello, world!");
+    println!("{}", ans.unwrap());
 }
 
-fn example_function (x: f64) -> f64 {
-    return x.powf(3.0) + 1.0;
+fn func (x: f64) -> f64 {
+    return 3.0 * x - 1.0;
+}
+
+fn recur (mut x_left: f64, mut x_right: f64) -> Option<f64> {
+    let x_mid: f64 = (x_left + x_right) * 0.5;
+    if func(x_mid).abs() < 1.0e-5 {
+        Some(x_mid) 
+    } else if func(x_mid) * func(x_left) < 0.0 {
+        x_right = x_mid;
+        recur(x_left, x_right)
+    } else if func(x_mid) * func(x_right) < 0.0 {
+        x_left = x_mid;
+        recur(x_left, x_right)
+    } else {
+        None
+    }
 }
